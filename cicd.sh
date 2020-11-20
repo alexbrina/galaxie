67a84b94c14d2c28794f312d0e2336f660b097f9
Alexandre Brina <alexbrina@gmail.com>
2020-11-20 09:13:51 -0300

    Introducing Galaxie template with azure pipeline and k8s deployment

---
+++ cicd.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+set -e
+
+function deploy
+{
+    cd $TRAVIS_BUILD_DIR/Template
+    
+    ARTIFACTS_FOLDER=./artifacts
+
+    echo "TRAVIS_BRANCH is: $TRAVIS_BRANCH"
+
+    rm -Rf $ARTIFACTS_FOLDER
+    mkdir $ARTIFACTS_FOLDER
+
+    echo "Copying..."
+    cp -Rvp ../Source/* ./content/
+
+    echo "Packing..."
+    nuget pack *.nuspec -NoDefaultExcludes -OutputDirectory $ARTIFACTS_FOLDER
+
+    echo "Pushing..."
+    dotnet nuget push $ARTIFACTS_FOLDER/*.nupkg --source https://api.nuget.org/v3/index.json --api-key $NUGET_API_KEY
+}
+
+$@
